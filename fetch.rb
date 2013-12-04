535ed5b50e485d74fcafc0f5a5a7623f42ccaaf3
Dave Vasilevsky <dave@vasilevsky.ca>
2013-12-04 12:20:58 -0500

    Add polls

--- fetch.rb
+++ fetch.rb
@@ -6,6 +6,7 @@ require 'pp'
 require 'csv'
 
 Candidate = Struct.new(:dept, :party, :name, :votes)
+Poll = Struct.new(:dept, :counted, :remaining)
 
 class Fetcher	
 	Site = 'http://siede.tse.hn/'
@@ -22,6 +23,10 @@ class Fetcher
 	def req_party(o)
 		req 'app.php/mapas/departamentosvotosdiputadosporpartido',
 			evento: o[:event], dept: o[:dept_id], partido: o[:party_id]
+	end
+	def req_poll(o)
+		req 'app.php/mapas/departamentalactasporid?evento=40&nivelelec=2&dept=1',
+			evento: o[:event], dept: o[:dept_id], nivelelec: 2
 	end
 	
 	def queue(builder, handler, **params)
@@ -43,10 +48,10 @@ class Fetcher
 		
 		doc.css('#seldepartamentos option').each do |el|
 			next if el.text.include?('Seleccione Departamento')
-			dept_name = el.text
-			dept_id = el.attr(:value)
-			queue(:req_dept, :handle_dept, event: event,
-				dept_id: el.attr(:value), dept_name: el.text)
+			opts = { event: event, dept_id: el.attr(:value),
+				dept_name: el.text }
+			queue(:req_dept, :handle_dept, **opts)
+			queue(:req_poll, :handle_poll, **opts)
 		end
 	end
 	def handle_dept(resp, o)
@@ -63,8 +68,18 @@ class Fetcher
 		JSON.parse(resp.body)['datadiputados'].each do |data|
 			cand = Candidate.new(o[:dept_name], o[:party_name],
 				data['nombre'], data['votos'])
-			@result_handler[cand]
+			@result_handler[:candidate, cand]
 		end
+	end
+	def handle_poll(resp, o)
+		done = todo = 0
+		JSON.parse(resp.body)['data']['data'].each do |data|
+			case data['name']
+				when /^Procesados:/ then done = data['y']
+				when /^Por procesar:/ then todo = data['y']
+			end
+		end
+		@result_handler[:poll, Poll.new(o[:dept_name], done, todo)]
 	end
 	
 	def initialize(&result_handler)
@@ -73,28 +88,41 @@ class Fetcher
 		queue(:req_base, :handle_base)
 	end
 	
-	def run
-		@hydra.run
-	end
+	def run; @hydra.run; end
 end
 
-class Writer	
-	def add(cand); @csv << cand.values; end
-	
-	def initialize(io, &block)
-		io = open(io, 'wb') unless io.respond_to?(:write)
-		begin
-			@csv = CSV.new(io)
-			@csv << Candidate.members
-			block[self]
-		ensure
-			io.close
+class Writer
+	class Data
+		def initialize(path, struct)
+			@rows = []
+			@csv = CSV.open(path, 'wb')
+			@csv << struct.members
+		end
+		def add(v); @rows << v.values; end
+		def write
+			@rows.sort.each { |r| @csv << r }
+			@csv.close
 		end
 	end
+	
+	def add(type, v); @data[type].add(v); end
+	def csv_init(name, struct)
+		path = File.join(@dir, name.to_s + 's.csv')
+		(@data ||= {})[name] = Data.new(path, struct)
+	end
+	
+	def initialize(dir, &block)
+		@dir = dir || '.'
+		csv_init(:candidate, Candidate)
+		csv_init(:poll, Poll)
+		block[self]
+		@data.values.each { |d| d.write }
+	end
 end
 
-Writer.new(ARGV.shift || $stdout) do |w|
-	f = Fetcher.new { |cand| w.add(cand) }
-	f.run
+#Typhoeus::Config.verbose = true
+
+Writer.new(ARGV.shift) do |w|
+	Fetcher.new { |*a| w.add(*a) }.run
 end
 
